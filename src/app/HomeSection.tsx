aadf98a3350b58457b49b6106f79e0e804571c04
Ahmed Bicir <ahmedhassanbicir.aa98@example.com>
2025-05-23 16:52:09 +0300

    removed home  page

--- src/app/HomeSection.tsx
+++
@@ -1,9 +0,0 @@
-export default function HomeSection() {
-  return (
-    <section id="home" className="w-full max-w-3xl mx-auto text-center py-16 px-4">
-      <h1 className="text-4xl font-bold mb-4">Hi, I'm a Front-End Developer</h1>
-      <p className="text-xl mb-2">4 years of hands-on experience crafting intuitive and responsive web interfaces using ReactJS, Typescript, NextJs, TailwindCSS, and Material UI.</p>
-      <p className="text-lg text-gray-700">I also have a strong background in back-end development with Python and Django, and a passion for machine learning. My interdisciplinary skills allow me to collaborate effectively and deliver high-performance, scalable, and user-centric applications.</p>
-    </section>
-  );
-} 
